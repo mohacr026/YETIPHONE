5d48902ec47a39f17388fc21bf9f30632e8f1c3f
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-11-28 10:53:03 +0100

    principio pagina principal

--- view/frontPage/frontPage.php
+++ view/frontPage/frontPage.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>YETiPhone</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    <h1>FRONT PAGE</h1>
+    <header>
+        <h1>YETiPhone</h1>
+        
+    </header>
 </body>
 </html>

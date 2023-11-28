ccba9d68409320df37af416e27e6bb4682a59393
efuentes21 <edgar.fuentes@inslapineda.cat>
2023-11-28 12:46:35 +0100

    Plantilla de clases

--- model/category.php
+++ model/category.php
@@ -2,9 +2,13 @@
 require_once("database.php");
 require_once("../controller/CategoryController.php");
 class Category extends Database {
+    //Attributes
     private $id;
     private $name;
+    private $parentCategory;
+    private $state;
 
+    
 
 }
 ?>
--- model/product.php
+++ model/product.php
@@ -2,13 +2,37 @@
 require_once("database.php");
 require_once("../controller/productController.php");
 class Product extends Database {
+    //Attributes
     private $id;
     private $name;
     private $description;
     private $category;
+    private $image;
+    private $price;
+    private $stock;
+    private $featured;
 
-    public function showAll(){
+    //Constructor
+    public function __construct($id, $name, $description, $category, $image, $price, $stock, $featured){
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->category = $category;
+        $this->image = $image;
+        $this->price = $price;
+        $this->stock = $stock;
+        $this->featured = $featured;
+    }
+
+    //Static methods
+    public static function displayAll(){
         
+    }
+    public static function displayAllFeatured(){
+        
+    }
+    public static function displayAllCategory($category){
+
     }
 }
 ?>

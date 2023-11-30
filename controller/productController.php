ffbecfe4fff1f72291fca69623d1648291332333
mohacr026-Portatil <mohamedcampusnet@gmail.com>
2023-11-30 10:28:21 +0100

    addProductos funcional (name, description)

--- controller/productController.php
+++ controller/productController.php
@@ -1,5 +1,6 @@
 <?php
-require_once("../model/database.php");
+require_once("./model/product.php");
+require_once("./model/database.php");  // Make sure to include the Database class file
 
 class ProductController {
     public function showFrontPageProducts(){
@@ -9,29 +10,48 @@ class ProductController {
     public function showAddProducts(){
         include("./view/adminProduct/addProduct.php");
     }
-    public function registerProduct(){
+
+    public function registerProduct() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $name = $_POST['name'];
             $description = $_POST['description'];
 
-            $db=Product::connect();
-
-            // Insertar el producto en la base de datos
-            $query = "INSERT INTO productos (name, description) VALUES ($1, $2)";
-            
-            $result = pg_query_params($db, $query, array(
-                $name,
-                $description
-            ));
-        
-            if ($result) {
-                echo "Producto agregado correctamente.";
-            } else {
-                echo "Error al agregar el producto: " . pg_last_error();
-            }
+            // instancia 
+            $product = new Product(null, $name, $description, null, null, null, null, null, true);
+            $this->insertProductIntoDatabase($product);
+        } else {
+            echo "The form was not submitted correctly.";
         }
     }
-    
-}
 
+    private function insertProductIntoDatabase(Product $product) {
+        $name = $product->getName();
+        $description = $product->getDescription();
+    
+        $db = Database::connect();  
+    
+        if (!$db) {
+            echo "Error connecting to the database.";
+            return;
+        }
+
+        try {
+            $query = "INSERT INTO product (name, description) VALUES (:name, :description)";
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':description', $description);
+            $result = $stmt->execute();
+
+            if ($result) {
+                echo "Product successfully registered in the database.";
+            } else {
+                echo "Error registering the product in the database.";
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        } finally {
+            $db = null;
+        }
+    }
+}
 ?>
--- model/product.php
+++ model/product.php
@@ -2,7 +2,7 @@
 require_once("database.php");
 
 class Product extends Database {
-    //Attributes
+    // Attributes
     private $id;
     private $name;
     private $description;
@@ -13,7 +13,7 @@ class Product extends Database {
     private $featured;
     private $isActive;
 
-    //Constructor
+    // Constructor
     public function __construct($id, $name, $description, $category, $image, $price, $stock, $featured, $isActive = true){
         $this->id = $id;
         $this->name = $name;
@@ -26,18 +26,96 @@ class Product extends Database {
         $this->isActive = $isActive;
     }
 
-    //Static methods
-    public static function displayAll(){
-        
-    }
-    public static function displayAllFeatured(){
-        
-    }
-    public static function displayAllCategory($category){
+    // Getters and Setters
 
+    public function getId() {
+        return $this->id;
     }
-    public insertProduct (){
 
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function getCategory() {
+        return $this->category;
+    }
+
+    public function setCategory($category) {
+        $this->category = $category;
+    }
+
+    public function getImage() {
+        return $this->image;
+    }
+
+    public function setImage($image) {
+        $this->image = $image;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+    public function getStock() {
+        return $this->stock;
+    }
+
+    public function setStock($stock) {
+        $this->stock = $stock;
+    }
+
+    public function getFeatured() {
+        return $this->featured;
+    }
+
+    public function setFeatured($featured) {
+        $this->featured = $featured;
+    }
+
+    public function getIsActive() {
+        return $this->isActive;
+    }
+
+    public function setIsActive($isActive) {
+        $this->isActive = $isActive;
+    }
+
+    // Static methods
+
+    public static function displayAll() {
+        // Implementar lógica para mostrar todos los productos
+    }
+
+    public static function displayAllFeatured() {
+        // Implementar lógica para mostrar todos los productos destacados
+    }
+
+    public static function displayAllCategory($category) {
+        // Implementar lógica para mostrar todos los productos de una categoría
+    }
+
+    public function insertProduct() {
+        // Implementar lógica para insertar un producto en la base de datos
     }
 }
 ?>

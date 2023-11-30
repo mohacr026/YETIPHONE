7d39d427ee05a0d7c287293d5f728c6dcaaf547d
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-11-30 15:37:19 +0100

    Insert de product movido al modelo

--- controller/productController.php
+++ controller/productController.php
@@ -16,7 +16,7 @@ class ProductController {
     }
 
     public function registerProduct() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (!empty($_POST)) {
             $name = $_POST['name'];
             $description = $_POST['description'];
             $price = $_POST['price'];
@@ -30,52 +30,9 @@ class ProductController {
             $this->uploadImage($product);
 
             // Insert the product into the database
-            $this->insertProductIntoDatabase($product);
+            $product->insertProductIntoDatabase();
         } else {
             echo "The form was not submitted correctly.";
-        }
-    }
-
-    private function insertProductIntoDatabase(Product $product) {
-        $name = $product->getName();
-        $description = $product->getDescription();
-        $price = $product->getPrice();
-        $id_category = $product->getCategory();
-        $stock = $product->getStock();
-        $isactive = $product->getIsactive();
-        $featured = $product->getFeatured();
-        $img = $product->getImage();
-
-        $db = Database::connect();  
-    
-        if (!$db) {
-            echo "Error connecting to the database.";
-            return;
-        }
-
-        try {
-            $query = "INSERT INTO product (name, description, price, id_category, stock, isactive, featured, img) VALUES (:name, :description, :price, :id_category, :stock, :isactive, :featured, :image)";
-            $stmt = $db->prepare($query);
-            $stmt->bindParam(':name', $name);
-            $stmt->bindParam(':description', $description);
-            $stmt->bindParam(':id_category', $id_category);
-            $stmt->bindParam(':price', $price);
-            $stmt->bindParam(':stock', $stock);
-            $stmt->bindParam(':isactive', $isactive);
-            $stmt->bindParam(':featured', $featured);
-            $stmt->bindParam(':image', $img);
-
-            $result = $stmt->execute();
-
-            if ($result) {
-                echo "Product successfully registered in the database.";
-            } else {
-                echo "Error registering the product in the database.";
-            }
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        } finally {
-            $db = null;
         }
     }
 

220134ff9fdc85a0efe8561d143041f231a16bfa
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-12-02 17:27:46 +0100

    Estilo de form aplicado a add products

--- view/adminCategory/addCategory.php
+++ view/adminCategory/addCategory.php
@@ -19,46 +19,48 @@
         <div class="container">
             <div class="formContainer">
                 <h2>Add a new category</h2>
-                <form action="index.php?controller=Category&action=registerCategory" method="POST">
-                    <div class="formRow">
-                        <label for="categoryId">Default Id:</label>
-                        <input type="text" id="categoryId" name="categoryId" readonly value="<?php echo "$defaultId";?>">
-                    </div>
+                <form action="index.php?controller=Category&action=registerCategory" method="POST" id="categoryForm">
+                    <div class="formColumn">
 
-                    <div class="formRow">
-                        <label for="name">Category name:</label>
-                        <input type="text" id="name" name="name" required>
-                    </div>
-
-                    <div class="formRowLine">
-                        <label for="showParent">Is a sub category:</label>
-                        <input type="checkbox" name="showParent" id="showParent">
-                    </div>
-
-                    <div class="formRow hiddenElement" id="parentCategoryDiv">
-                        <label for="parent">Parent category</label>
-                        <select id="parent" name="parent">
-                            <option value=""></option>
-                            <?php
-                                if(isset($parentCategories)){
-                                    if(empty($parentCategories)){
-                                        echo "<option value='nothing' selected>There are no parent categories</option>";
-                                    } else {
-                                        foreach ($parentCategories as $parentCategory) {
-                                            $id = $parentCategory['id'];
-                                            $name = $parentCategory['name'];
-                                            echo "<option value='$id'>$name</option>";
+                        <div class="formRow">
+                            <label for="categoryId">Default Id:</label>
+                            <input type="text" id="categoryId" name="categoryId" readonly value="<?php echo "$defaultId";?>">
+                        </div>
+    
+                        <div class="formRow">
+                            <label for="name">Category name:</label>
+                            <input type="text" id="name" name="name" required>
+                        </div>
+    
+                        <div class="formRowLine">
+                            <label for="showParent">Is a sub category:</label>
+                            <input type="checkbox" name="showParent" id="showParent">
+                        </div>
+    
+                        <div class="formRow hiddenElement" id="parentCategoryDiv">
+                            <label for="parent">Parent category</label>
+                            <select id="parent" name="parent">
+                                <option value=""></option>
+                                <?php
+                                    if(isset($parentCategories)){
+                                        if(empty($parentCategories)){
+                                            echo "<option value='nothing' selected>There are no parent categories</option>";
+                                        } else {
+                                            foreach ($parentCategories as $parentCategory) {
+                                                $id = $parentCategory['id'];
+                                                $name = $parentCategory['name'];
+                                                echo "<option value='$id'>$name</option>";
+                                            }
                                         }
                                     }
-                                }
-                            ?>
-                        </select>
-                    </div>
-
-                    <div class="formAction">
-                        <input type="submit" value="Add category" class="addBtn">
+                                ?>
+                            </select>
+                        </div>
                     </div>
                 </form>
+                <div class="formAction">
+                    <button type="submit" value="Add category" class="addBtn" form="categoryForm">Add category</button>
+                </div>
             </div>
         </div>
     </main>
--- view/adminProduct/addProduct.php
+++ view/adminProduct/addProduct.php
@@ -10,69 +10,79 @@
     <?php
     include("./view/components/header.php");
     ?>
+    <main>
+        <aside>
 
-    <h2>Add Product</h2>
+        </aside>
+        <div class="container">
+            <div class="formContainer">
+                <h2>Add a new product</h2>
+                <form action="index.php?controller=Product&action=registerProduct" method="post" id="productForm">
+                    <div class="formColumn">
+                        <div class="formRow">
+                            <label for="name">Product Name:</label>
+                            <input type="text" id="name" name="name" required>
+                        </div>
+    
+                        <div class="formRow">
+                            <label for="description">Description:</label>
+                            <textarea id="description" name="description" rows="4" required></textarea>
+                        </div>
+    
+                        <div class="formRow">
+                            <label for="price">Price:</label>
+                            <input type="number" id="price" name="price" min="1" step="1" required>
+                        </div>
+                    </div>
+                    <div class="formColumn">
 
-    <form action="index.php?controller=Product&action=registerProduct" method="post">
-
-        <label for="name">Product Name:</label>
-        <input type="text" id="name" name="name" required>
-        <br>
-
-        <label for="description">Description:</label>
-        <textarea id="description" name="description" rows="4" required></textarea>
-        <br>
-
-        <label for="price">Price:</label>
-        <input type="number" id="price" name="price" min="1" step="1" required>
-        <br>
-
-        <label for="category">Category:</label>
-        <select id="category" name="category" required>
-            <?php
-            $db = Database::connect();
-            $query = "SELECT id, name FROM category WHERE isActive = true";
-            $stmt = $db->prepare($query);
-            $stmt->execute();
-            $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            foreach ($categories as $category):
-            ?>
-                <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
-            <?php endforeach; ?>
-        </select>
-        <br>
-
-        <label for="stock">Stock:</label>
-        <input type="number" id="stock" name="stock" min="0" required>
-        <br>
-
-        <label for="image">Product Image:</label>
-        <input type="file" id="image" name="image" accept="image/*" required>
-        <br>
-        <!-- 
-
-
-
-
-
-
-        <label for="featured">Featured:</label>
-        <select id="featured" name="featured" required>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-        </select>
-        <br>
-
-        <label for="isactive">Is Active:</label>
-        <select id="isactive" name="isactive" required>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-        </select>
-        <br> -->
-
-        <input type="submit" value="Add Product">
-    </form>
-
+                        <div class="formRow">
+                            <label for="category">Category:</label>
+                            <select id="category" name="category" required>
+                                <?php
+                                $db = Database::connect();
+                                $query = "SELECT id, name FROM category WHERE isActive = true";
+                                $stmt = $db->prepare($query);
+                                $stmt->execute();
+                                $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+                                foreach ($categories as $category):
+                                ?>
+                                    <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+    
+                        <div class="formRow">
+                            <label for="stock">Stock:</label>
+                            <input type="number" id="stock" name="stock" min="0" required>
+                        </div>
+    
+                        <div class="formRow">
+                            <label for="image">Product Image:</label>
+                            <input type="file" id="image" name="image" accept="image/*" required>
+                        </div>
+                    </div>
+                    
+                    <!-- 
+                    <label for="featured">Featured:</label>
+                    <select id="featured" name="featured" required>
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </select>
+                    <br>
+                    <label for="isactive">Is Active:</label>
+                    <select id="isactive" name="isactive" required>
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </select>
+                    <br> -->
+                </form>
+                <div class="formAction">
+                    <button type="submit" value="Add Product" class="addBtn" form="productForm">Add product</button>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>

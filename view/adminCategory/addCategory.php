b7df06b1449882c9460ebefaf5cd008acea9eb3c
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-12-04 09:19:17 +0100

    Añadido favicon a todas las páginas

--- view/adminCategory/addCategory.php
+++ view/adminCategory/addCategory.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>YETiPhone</title>
     <link rel="stylesheet" href="css/main.css">
+    <link rel="shortcut icon" href="./src/img/snowflake.png" type="image/x-icon">
     <script src="./src/js/categoryCheckHider.js"></script>
 </head>
 <body>

a745563c110c93788e3c4ccb93cf7dcee82ffdd9
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-01-22 14:22:34 +0100

    Pagina cat casi acabada

--- view/components/productCardComponent.php
+++ view/components/productCardComponent.php
@@ -1,15 +1,32 @@
 <article class="productCardContainer">
-    <h3><?= $product->getName(); ?></h3>
+    <div class="cardTop">
+        <a href="index.php?controller=Product&action=showProductPage&product=<?= urlencode(serialize($product)) ?>&category=<?= $product->getCategory(); ?>"><h3><?= $product->getName(); ?></h3></a>
+        <?php if($product->getFeatured()): ?>
+        <div>
+            <p>ON SALE!!</p>
+        </div>
+        <?php endif; ?>
+    </div>
     <div class="separator"></div>
     <div class="productCardInfo">
         <img class="productImage" src="./src/img/products/<?php echo $product->getImage()[0];?>" alt="<?php $product->getName();?>">
         <div class="info">
-            <p>Memory: <?= $product->getMemory(); ?></p>
-            <p>Storage: <?= $product->getStorage(); ?></p>
+            <div class="spec">
+                <p>Memory:</p>
+                <p><?= $product->getMemory(); ?>GB</p>
+            </div>
+            <div class="spec">
+                <p>Storage:</p>
+                <p><?= $product->getStorage(); ?>GB</p>
+            </div>
         </div>
         <div class="price">
-            <p><?= $product->getPrice(); ?> €</p>
+            <p><?= $product->getPrice(); ?>€</p>
+            <button>
+                <img class="cart" src="./src/img/shoppingCartBlue.png" alt="add to cart">
+                ADD TO CART
+            </button>
         </div>
     </div>
-    <a href="index.php?controller=Product&action=showProductPage&product=<?= urlencode(serialize($product)) ?>&category=<?= $product->getCategory(); ?>">ADD TO CART</a>
+    
 </article>

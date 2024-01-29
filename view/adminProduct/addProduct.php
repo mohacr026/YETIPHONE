0d63335817800e530e6118f76d7675af5470c33e
efuentes21 <edgar.fuentes@inslapineda.cat>
2024-01-29 21:57:53 +0100

    Miniupdate accesibility

--- view/adminProduct/addProduct.php
+++ view/adminProduct/addProduct.php
@@ -27,77 +27,77 @@
                 <form action="index.php?controller=Product&action=registerProduct" method="post" id="productForm" enctype="multipart/form-data">
                     <div class="formColumn">
                         <div class="formRow">
-                            <label for="name">Product Name:</label>
-                            <input type="text" id="name" name="name" required min="2">
+                            <label for="name" aria-label="Product name">Product Name:</label>
+                            <input type="text" id="name" name="name" required min="2" aria-required="true" tabindex="60">
                         </div>
-    
+
                         <div class="formRow">
-                            <label for="description">Description:</label>
-                            <textarea id="description" name="description" rows="4" required></textarea>
+                            <label for="description" aria-label="Product description">Description:</label>
+                            <textarea id="description" name="description" rows="4" required aria-required="true" tabindex="61"></textarea>
                         </div>
-    
+
                     </div>
                     <div class="formColumn">
 
                         <div class="formRow">
-                            <label for="id_category">Category:</label>
-                            <select id="category" name="id_category" required>
+                            <label for="id_category" aria-label="Product category">Category:</label>
+                            <select id="category" name="id_category" required aria-required="true" tabindex="62">
                                 <?php foreach ($categories as $category): ?>
                                     <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
-                        
+
                         <div class="formRow">
-                            <label for="price">Price:</label>
-                            <input type="number" id="price" name="price" min="1" step="1" required>
+                            <label for="price" aria-label="Product price">Price:</label>
+                            <input type="number" id="price" name="price" min="1" step="1" required aria-required="true" tabindex="63">
                         </div>
 
                         <div class="formRow">
-                            <label for="storage">Storage:</label>
-                            <input type="number" id="storage" name="storage" min="0" required>
+                            <label for="storage" aria-label="Product storage">Storage:</label>
+                            <input type="number" id="storage" name="storage" min="0" required aria-required="true" tabindex="64">
                         </div>
 
                         <div class="formRow">
-                            <label for="memory">Memory:</label>
-                            <input type="number" id="memory" name="memory" min="0" required>
+                            <label for="memory" aria-label="Product memory">Memory:</label>
+                            <input type="number" id="memory" name="memory" min="0" required aria-required="true" tabindex="65">
                         </div>
 
                         <div class="formRow">
-                            <label for="stock">Stock:</label>
-                            <input type="number" id="stock" name="stock" min="0" required>
+                            <label for="stock" aria-label="Product stock">Stock:</label>
+                            <input type="number" id="stock" name="stock" min="0" required aria-required="true" tabindex="66">
                         </div>
 
                         <div class="formRow">
-                            <label for="colors">Colors:</label>
-                            <input type="text" id="colors" name="colors" placeholder="#FFFFFF, #000000, #FF00FF..." title="Write each color code separated with commas as shown in the example" required>
-                        </div>
-    
-                        <div class="formRow">
-                            <label for="img">Product Image:</label>
-                            <input type="file" id="img" name="img[]" accept="image/jpeg, image/png" multiple>
+                            <label for="colors" aria-label="Product colors">Colors:</label>
+                            <input type="text" id="colors" name="colors" placeholder="#FFFFFF, #000000, #FF00FF..." title="Write each color code separated with commas as shown in the example" required aria-required="true" tabindex="67">
                         </div>
 
                         <div class="formRow">
-                            <label for="featured">Featured:</label>
-                            <select id="featured" name="featured" required>
+                            <label for="img" aria-label="Product images">Product Image:</label>
+                            <input type="file" id="img" name="img[]" accept="image/jpeg, image/png" multiple tabindex="68">
+                        </div>
+
+                        <div class="formRow">
+                            <label for="featured" aria-label="Is Featured?">Featured:</label>
+                            <select id="featured" name="featured" required aria-required="true" tabindex="69">
                                 <option value="true">True</option>
                                 <option value="false">False</option>
                             </select>
                         </div>
 
                         <div class="formRow">
-                            <label for="isactive">Active:</label>
-                            <select id="isactive" name="isactive" required>
-                                <option value="true">True</option>
-                                <option value="false">False</option>
+                            <label for="isactive" aria-label="Is active?">Active:</label>
+                            <select id="isactive" name="isactive" required aria-required="true" tabindex="70">
+                                <option value="true" tabindex="71">True</option>
+                                <option value="false" tabindex="72">False</option>
                             </select>
                         </div>
 
                     </div>
                 </form>
                 <div class="formAction">
-                    <button type="submit" value="Add Product" class="addBtn" form="productForm">Add product</button>
+                    <button type="submit" value="Add Product" class="addBtn" form="productForm" tabindex="73">Add product</button>
                 </div>
             </div>
         </div>

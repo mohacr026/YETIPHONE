ff700bdfd245b11770e7f494b418a98549c01452
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-02-21 10:39:03 +0100

    cambiador de fotos

---
+++ src/js/imageChanger.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(){
+    let bigImage = document.getElementById("productPageShowingImage");
+
+    let sideImages = document.getElementsByClassName("sideImage");
+
+    for (let i = 0; i < sideImages.length; i++) {
+        const image = sideImages[i];
+        image.addEventListener("click", function(){
+            bigImage.src = image.src;
+        })
+    }
+})
